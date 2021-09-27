f47c78b95cd4fcb66dedfc4d23234caf7bb563aa
goodvvinn <goodvvinn@gmail.com>
2021-09-27 12:00:28 +0300

    Deleting functionality added

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,11 +1,13 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agents";
-import { Profile } from "../models/profile";
+import { Photo, Profile } from "../models/profile";
 import { store } from "./store";
 
 export default class ProfileStore{
     profile: Profile | null = null;
     loadingProfile = false;
+    uploading = false;
+    loading = false;
     
     constructor() {
        makeAutoObservable(this)
@@ -30,6 +32,61 @@ export default class ProfileStore{
         } catch (error) {
             console.log(error)
             runInAction(() => this.loadingProfile = false);
+        }
+    }
+    uploadPhoto = async (file: Blob) => {
+        this.uploading = true;
+        try {
+                const response = await agent.Profiles.uploadPhoto(file);
+                const photo = response.data;
+                runInAction(() => {
+                    if (this.profile) {
+                        this.profile.photos?.push(photo);
+                        if (photo.isMain && store.userStore.user) {
+                            store.userStore.setImage(photo.url);
+                            this.profile.image = photo.url;
+                        }
+                    }
+                })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => this.uploading = false);
+        }
+        this.uploading = false;
+    }
+
+    setMainPhoto = async (photo: Photo) => {
+        this.loading = true;
+        try {
+            agent.Profiles.setMainPhoto(photo.id);
+            store.userStore.setImage(photo.url);
+            runInAction(() => {
+                if (this.profile && this.profile.photos) {
+                    this.profile.photos.find(p => p.isMain)!.isMain = false
+                    this.profile.photos.find(p => p.id === photo.id)!.isMain = true;
+                    this.profile.image = photo.url;
+                    this.loading = false;
+                }
+            })
+        } catch (error) {
+            runInAction(() => this.loading = false)
+            console.log(error)
+        }
+    }
+
+    deletePhoto = async (photo: Photo) => {
+        this.loading = true;
+        try {
+            await agent.Profiles.deletePhoto(photo.id);
+            runInAction(() => {
+                if (this.profile) {
+                    this.profile.photos = this.profile.photos?.filter(p => p.id !== photo.id);
+                    this.loading = false;
+                }
+            })
+        } catch (error) {
+            runInAction(() => this.loading = false)
+            console.log(error)
         }
     }
 }
