3f172378d0413dba93b0a25e3102a51c73e6baf3
goodvvinn <goodvvinn@gmail.com>
2021-10-04 16:13:01 +0300

    PreProduct build

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -12,11 +12,11 @@ export default observer(function HomePage() {
             <Container text>
                 <Header as='h1' inverted>
                     <Image size='massive' src='/assets/logo.png' style={{ marginBottom: 12 }} />
-                    Reactivities
+                    Activity Hub
                 </Header>
                 {userStore.isLoggedIn ? (
                     <>
-                        <Header as='h2' inverted content='Welcome to Reactivities!' />
+                        <Header as='h2' inverted content='Welcome to Global Activity Store!' />
                         <Button as={Link} to='/activities' size='huge' inverted>
                             Go to Activities!!!
                         </Button>
