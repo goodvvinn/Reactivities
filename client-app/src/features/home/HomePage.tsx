9928d3524908fc2000152679fe8a7cb52e55ff29
goodvvinn <goodvvinn@gmail.com>
2021-10-04 15:27:04 +0300

    HomePage style fixed

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -8,7 +8,7 @@ import RegisterForm from "../users/RegisterForm";
 export default observer(function HomePage() {
     const { userStore, modalStore } = useStore();
     return (
-        <Segment inverted textaligin='center' vertical className="masthead">
+        <Segment inverted textAlign='center' vertical className="masthead">
             <Container text>
                 <Header as='h1' inverted>
                     <Image size='massive' src='/assets/logo.png' style={{ marginBottom: 12 }} />
