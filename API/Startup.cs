8b2cf571276de798809e769aac653843b337b9b8
goodvvinn <goodvvinn@gmail.com>
2021-10-04 16:56:30 +0300

    Postgres implemented

--- API/Startup.cs
+++ API/Startup.cs
@@ -52,6 +52,10 @@ namespace API
 
             // app.UseHttpsRedirection();
             app.UseRouting();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseCors("CorsPolicy");
 
             app.UseAuthentication();
@@ -61,6 +65,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<ChatHub>("/chat");
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
