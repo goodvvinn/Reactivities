1ec8029d084b0fd198c8874763f03a9965446da2
goodvvinn <goodvvinn@gmail.com>
2021-10-06 17:23:04 +0300

    FИ Login Complete

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -31,6 +31,7 @@ export default observer(function HomePage() {
                         </Button>
                         <Divider horizontal inverted> Or </Divider>
                         <Button 
+                            loading={userStore.fbLoading}
                             size='huge'
                             inverted
                             color='facebook'
