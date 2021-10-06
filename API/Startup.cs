dd982b177593b17f986b097a40da1e5e9a8207e8
goodvvinn <goodvvinn@gmail.com>
2021-10-06 18:00:03 +0300

    Restrictions realozed

--- API/Startup.cs
+++ API/Startup.cs
@@ -55,8 +55,8 @@ namespace API
             .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
             .FormActions(s => s.Self())
             .FrameAncestors(s => s.Self())
-            .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com/"))
-            .ScriptSources(s => s.Self().CustomSources("sha256-UGmU/l9u6P133spP/KgqcB6Pm5dMtQEjQQTiEFHTBwI=")));
+            .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com/", "https://www.facebook.com"))
+            .ScriptSources(s => s.Self().CustomSources("sha256-UGmU/l9u6P133spP/KgqcB6Pm5dMtQEjQQTiEFHTBwI=", "sha256-UGmU/l9u6P133spP/KgqcB6Pm5dMtQEjQQTiEFHTBwI=")));
 
             if (env.IsDevelopment())
             {
